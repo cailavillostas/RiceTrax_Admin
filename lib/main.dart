c6e18ee269e8ca9b9c903f4c7ac985b1565860d2
cailavillostas <cailavillostas@gmsil.com>
2025-07-30 07:46:27 +0800

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,30 @@
 import 'package:flutter/material.dart';
-import 'package:rice_trax/Dashboard.dart';
-import 'package:rice_trax/Inventory.dart';
-import 'package:rice_trax/RiceStock.dart';
+import 'Dashboard.dart';
+import 'Inventory.dart';
+import 'RiceStock.dart';
+import 'Login.dart';
+import 'SignUp.dart';
+import 'ChangePassword.dart';
 
 void main() {
   runApp(RiceTraxApp());
 }
 
-
 class RiceTraxApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Dashboard(),
+      title: 'RiceTrax',
       debugShowCheckedModeBanner: false,
+      initialRoute: '/login',
+      routes: {
+        '/login': (context) => LoginScreen(),
+        '/signup': (context) => SignUpScreen(),
+        '/change_password': (context) => ChangePasswordScreen(),
+        '/dashboard': (context) => Dashboard(),
+        '/inventory': (context) => Inventory(),
+        '/rice_stock': (context) => RiceStock(),
+      },
     );
   }
 }
